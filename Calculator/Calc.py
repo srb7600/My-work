0ab7bac1b38683ef976c2e361ca1dcdb78166a54
Saurabh Kumar <saurabh.kumar@expleogroup.com>
2025-08-06 12:39:48 +0100

    Update

--- Calculator/Calc.py
+++ Calculator/Calc.py
@@ -28,28 +28,24 @@ operations = {
 def calculator():
     print(art.logo)
     should_accumulate = True
-    num1 = float(input("What is the first number?: "))
+    num1 = float(input("Enter first number: "))
 
     while should_accumulate:
         for symbol in operations:
             print(symbol)
-        operation_symbol = input("Pick an operation: ")
-        num2 = float(input("What is the next number?: "))
+        operation_symbol = input("What do you want to do? ")
+        num2 = float(input("Enter next number?: "))
         answer = operations[operation_symbol](num1, num2)
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ")
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit the calculator.")
 
         if choice == "y":
             num1 = answer
         else:
             should_accumulate = False
-            print("\nThank you for using the calculator.")
-            #calculator()
-
+            print("\nThank you for using the calculator. See you again.")
 
 calculator()
-#def add(num1, num2):
-  #  return num1 + num2
 
 
